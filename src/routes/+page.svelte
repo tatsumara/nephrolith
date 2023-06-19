04a762364d4054a19d8f64ee76c7d33cd1b468d9
tatsumara <mara@tatsumara.dev>
2023-06-20 01:15:05 +0200

    feat: add tags and collections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,16 @@
 
 <div class="box-container">
 	{#each collection as monster}
-	<h1>{monster.name}</h1>
+		<div class="box">
+			<h1>{monster.name}</h1>
+			{#if monster.collection}
+				<p>{monster.collection}</p>
+			{/if}
+			{#if monster.tags}
+				{#each monster.tags as tag}
+					<p>{tag}</p>
+				{/each}
+			{/if}
+		</div>
 	{/each}
 </div>
